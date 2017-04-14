868221230d4bc30a3ffd9f2ff67d7c5e75a60369
tomoki.murakami <tomoki.murakami@mixi.co.jp>
2017-04-14 15:21:08 +0900

    calc test

--- spec/calc_area_spec.rb
+++ spec/calc_area_spec.rb
@@ -2,11 +2,23 @@ require 'calc_area'
 require 'spec_helper'
 
 describe CalcArea do
-  it 'standard input' do
-    calcArea = CalcArea.new
-    calcArea.getInputs
-    calcArea.filterInput
-
-    expect(calcArea.checkInput).to be true
+  before do
+    @calcArea = CalcArea.new
+    @calcArea.getInputs
+    @calcArea.filterInput
   end
+
+  it 'standard input' do
+    expect(@calcArea.checkInput).to be true
+  end
+
+  it 'calculate radius' do
+    @calcArea.calculate
+
+    @calcArea.result.each_with_index do |r,i|
+      src_input = @calcArea.valid_inputs[i]
+      expect(r).to eq (src_input * src_input * 3.14)
+    end
+  end
+  
 end
